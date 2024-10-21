d2247b1b5f83bd686dbfea3cfe3137dbdb592cd7
marco.radovancovici <marco.radovancovici@gmail.com>
2024-10-21 21:57:14 +0000

    Disable test to showcase

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,10 +2,10 @@
 import pytest
 from src.app import add, multiply  # Import from the `src` package
 
-def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+# def test_add():
+#     assert add(2, 3) == 5
+#     assert add(-1, 1) == 0
+#     assert add(0, 0) == 0
 
 def test_multiply():
     assert multiply(2, 3) == 6
