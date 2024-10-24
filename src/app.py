6a43a09eaba0b18d0d4ad62a57653a2527aab1bb
marco.radovancovici <marco.radovancovici@gmail.com>
2024-10-24 13:53:34 +0000

    100% coverage of code

--- src/app.py
+++ src/app.py
@@ -10,4 +10,14 @@ def add(a, b):
     return a + b
 
 def multiply(a, b):
+    """
+    Multiply two numbers together
+
+    Args:
+        a (int): First number
+        b (int): Seconds number
+
+    Returns:
+        (int): Product of a and b
+    """
     return a * b
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,20 +2,20 @@
 import pytest
 from src.app import add, multiply  # Import from the `src` package
 
-# def test_add():
-#     assert add(2, 3) == 5
-#     assert add(-1, 1) == 0
-#     assert add(0, 0) == 0
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
 
 def test_multiply():
     assert multiply(2, 3) == 6
     assert multiply(-1, 1) == -1
     assert multiply(0, 5) == 0
 
-# @pytest.mark.parametrize("a,b", [(None, 1), (1, None), (None, None)])
-# def test_add_invalid_inputs(a, b):
-#     with pytest.raises(TypeError):
-#         add(a, b)
+@pytest.mark.parametrize("a,b", [(None, 1), (1, None), (None, None)])
+def test_add_invalid_inputs(a, b):
+    with pytest.raises(TypeError):
+        add(a, b)
 
 @pytest.mark.parametrize("a,b", [(None, 1), (1, None), (None, None)])
 def test_multiply_invalid_inputs(a, b):
